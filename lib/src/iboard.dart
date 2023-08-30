450c970aa66003a9775272680b598563ff9e3071
Cosminflv <cosmin_popovici2012@yahoo.com>
2023-08-30 16:18:07 +0300

    Added IBoard constructor.

--- lib/src/iboard.dart
+++ lib/src/iboard.dart
@@ -5,4 +5,7 @@ import 'package:tic_tac_toe_lib/src/board.dart';
 abstract class IBoard {
   void placePiece(Position pos, Piece piece);
   List<List<Piece>> get board;
+  IBoard() {
+    Board();
+  }
 }
